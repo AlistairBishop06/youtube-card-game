d032aeace7abc9cca02e2f3a0bf7985e142892ba
Alistair Bishop <alistairbishop@gmx.co.uk>
2026-04-06 00:46:02 +0100

    Delete config.js

--- js/config.js
+++
@@ -1,5 +0,0 @@
-/**
- * Add your YouTube Data API v3 key (Browser / HTTP referrer restricted).
- * https://console.cloud.google.com/apis/credentials
- */
-export const YOUTUBE_API_KEY = "AIzaSyArqKXtrnU6l8SdnxsuguxOs2rKp4yrTYs";
